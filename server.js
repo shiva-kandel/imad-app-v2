c812856d1c6ec86a74ce40c171a22a43b396ddfa
shiva-kandel <shivakandel12345.sk@gmail.com>
2017-02-21 13:00:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('artical-one',function(req,res){
+res.send('article one requested will beserve here');
+    });
+    app.get('artical-two',function(req,res){
+res.send('article two requested will beserve here');
+    });app.get('artical-three',function(req,res){
+res.send('article three requested will beserve here');
+    });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
